7e61b639a42cf614d62a46dcd4e64a0abfc23939
jgraven <graven1@illinois.edu>
2014-05-11 12:17:02 -0500

    Final
    Pretty much done.  It saves files and stuff.  Still terrible at Android
    UI

--- Fuzzer/src/com/example/fuzzer/MainActivity.java
+++ Fuzzer/src/com/example/fuzzer/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.fuzzer;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.EnumMap;
 import java.util.Map;
 import java.util.Random;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
@@ -14,6 +21,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
@@ -26,11 +34,10 @@ import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 
 public class MainActivity extends ActionBarActivity {
-	public final static String EXTRA_INPUT = "com.example.fuzzer.INPUT";
-	public final static String EXTRA_TYPE = "com.example.fuzzer.TYPE";
 	private static final int WHITE = 0xFFFFFFFF;
     private static final int BLACK = 0xFF000000;
 	public String barcode_type = "@string/qr_code";
+	public String current_barcode_string = "";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +55,7 @@ public class MainActivity extends ActionBarActivity {
 		TextView tv = (TextView) findViewById(R.id.barcode_text);
 		EditText editText = (EditText) findViewById(R.id.edit_input);
 		String input = editText.getText().toString();
+		current_barcode_string = input;
 		String text = null;
 		if (input.length() > 30){
 			text = input.substring(0, 30).concat("... (Total length: )").concat(Integer.toString(input.length()));
@@ -74,16 +82,41 @@ public class MainActivity extends ActionBarActivity {
 	}
 	
 	public void randomToBarcode(View view) {
+		//Grab necessary info from the layout
 		ImageView iv = (ImageView) findViewById(R.id.barcode_view);
 		TextView tv = (TextView) findViewById(R.id.barcode_text);
 		EditText editText = (EditText) findViewById(R.id.code_length);
 		int length =  Integer.parseInt(editText.getText().toString());
+		final CheckBox upperBox = (CheckBox) findViewById(R.id.upper_check);
+		final CheckBox lowerBox = (CheckBox) findViewById(R.id.lower_check);
+		final CheckBox numBox = (CheckBox) findViewById(R.id.num_check);
+		final CheckBox specialBox = (CheckBox) findViewById(R.id.special_check);
+
+		//Setup character space for random generation
 		String lower = "abcdefghijklmnopqurstuvwxyz";
 		String upper = "ABCDEFGHIJKLMNOPQURSTUVWXYZ";
 		String num = "123456789";
 		String special = "!@#$%&^*()-_=+`~[]{};,.?/:\"\'\\";
 		String charspace = "";
-		charspace += special;
+		
+		//Read checkboxes to determine what to add to charspace
+		if (upperBox.isChecked()) {
+			charspace += upper;
+		}
+		if (lowerBox.isChecked()) {
+			charspace += lower;
+		}
+		if (numBox.isChecked()) {
+			charspace += num;
+		}
+		if (specialBox.isChecked()) {
+			charspace += special;
+		}
+		if (!upperBox.isChecked() && !lowerBox.isChecked() &&
+				!numBox.isChecked() && !specialBox.isChecked()) {
+			charspace = lower;
+		}
+		
 		char[] chars = charspace.toCharArray();
 		StringBuilder sb = new StringBuilder();
 		Random random = new Random();
@@ -92,6 +125,7 @@ public class MainActivity extends ActionBarActivity {
 			sb.append(c);
 		}
 		String input = sb.toString();
+		current_barcode_string = input;
 		String text = null;
 		if (input.length() > 30){
 			text = input.substring(0, 30).concat("... \nTotal length: ").concat(Integer.toString(input.length()));
@@ -103,10 +137,10 @@ public class MainActivity extends ActionBarActivity {
 
 	        switch(barcode_type) {
 	        case "@string/qr_code": 
-	        	bitmap = encodeAsBitmap(input, BarcodeFormat.QR_CODE, 300, 150);
+	        	bitmap = encodeAsBitmap(input, BarcodeFormat.QR_CODE, 550, 550);
 	        	break;
 	        case "@string/code_128":
-	        	bitmap = encodeAsBitmap(input, BarcodeFormat.CODE_128, 300, 150);
+	        	bitmap = encodeAsBitmap(input, BarcodeFormat.CODE_128, 550, 550);
 	        	break;
 	        }
 
@@ -121,19 +155,88 @@ public class MainActivity extends ActionBarActivity {
 	public void onTypeSelected(View view) {
 		boolean checked = ((RadioButton) view).isChecked();
 		
-		switch(view.getId()) {
-		case R.id.qr_select:
+		int id = view.getId();
+		if (id == R.id.qr_select) {
 			if (checked)
 				barcode_type = "@string/qr_code";
-			break;
-		case R.id.code128_select:
+		} else if (id == R.id.code128_select) {
 			if (checked)
 				barcode_type = "@string/code_128";
-			break;
 		}
 	}
 	
-	Bitmap encodeAsBitmap(String contents, BarcodeFormat format, int img_width, int img_height) throws WriterException {
+	
+	//Helper functions for saving barcodes and strings
+	public boolean isExternalStorageWritable() {
+		String state = Environment.getExternalStorageState();
+		if (Environment.MEDIA_MOUNTED.equals(state)) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isExternalStorageReadable() {
+	    String state = Environment.getExternalStorageState();
+	    if (Environment.MEDIA_MOUNTED.equals(state) ||
+	        Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+	        return true;
+	    }
+	    return false;
+	}
+	
+	public void saveBarcode(View view) {
+		//Pull image info from ImageView, make byte array from it
+		ImageView iv = (ImageView) findViewById(R.id.barcode_view);
+		Drawable drawable = iv.getDrawable();
+		BitmapDrawable bitmapDrawable = ((BitmapDrawable) drawable);
+		Bitmap bitmap = bitmapDrawable.getBitmap();
+		ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+		bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+				
+		//Grab filename from the text field
+		EditText editText = (EditText) findViewById(R.id.file_name);
+		String name = editText.getText().toString();
+		
+		//Open filestream and write the image
+		String root = Environment.getExternalStorageDirectory().toString();
+	    File myDir = new File(root + "/saved_images");    
+	    myDir.mkdirs();
+	    Random generator = new Random();
+	    int n = 10000;
+	    n = generator.nextInt(n);
+	    String fnamePic = name + n + ".jpg";
+	    String fnameText = name + n + ".txt";
+	    File filePic = new File (myDir, fnamePic);
+	    if (filePic.exists ()) filePic.delete (); 
+	    try {
+	           FileOutputStream out = new FileOutputStream(filePic);
+	           bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+	           out.flush();
+	           out.close();
+
+	    } catch (Exception e) {
+	           e.printStackTrace();
+	    }
+	    File fileText = new File (myDir, fnameText);
+	    if (fileText.exists ()) fileText.delete (); 
+	    try {
+	           FileOutputStream out = new FileOutputStream(fileText);
+	           out.write(current_barcode_string.getBytes());
+	           out.flush();
+	           out.close();
+
+	    } catch (Exception e) {
+	           e.printStackTrace();
+	    }
+		
+		//Start media scan
+		Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+		Uri contentUri = Uri.fromFile(filePic);
+		mediaScanIntent.setData(contentUri);
+		this.sendBroadcast(mediaScanIntent); 
+	}
+	
+	public Bitmap encodeAsBitmap(String contents, BarcodeFormat format, int img_width, int img_height) throws WriterException {
 	    String contentsToEncode = contents;
 	    if (contentsToEncode == null) {
 	        return null;
